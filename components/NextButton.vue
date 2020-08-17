5bbb02d29dbed335d1b419c6a8b71a7869138953
Jouan Chen <joannecja@gmail.com>
2020-08-17 20:11:00 +0800

    add next button

---
+++ components/NextButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button
+    class="next-btn"
+    @click="$emit('handleClick', words)"
+  >
+    Next
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    words: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.next-btn {
+  margin-bottom: 20px;
+  height: 25px;
+  width: 100px;
+  font-size: 15px;
+  background-color: #ef959d;
+  border-color: #ef959d;
+  border-radius: 5%;
+}
+</style>
--- pages/hangman.vue
+++ pages/hangman.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="container">
     <QuizBoard :word="word" :alphabet="alphabet" />
+    <NextButton v-if="isFinished" :words="words" @handleClick="randomNewWord" />
     <AlphabetBoard :alphabet="alphabet" :disableclick="disableClick" />
   </section>
 </template>
@@ -46,6 +47,21 @@ export default {
         Y: false,
         Z: false
       }
+    }
+  },
+  computed: {
+    isFinished () {
+      let finished = true
+      const letters = this.word.split('')
+      letters.forEach((letter) => {
+        const show = this.alphabet.[letter]
+        if (!show) { // if any correct letter not show up, keep playing
+          finished = false
+          return false
+        }
+      })
+
+      return finished
     }
   },
   created () {
@@ -95,16 +111,16 @@ export default {
 </script>
 
 <style lang="scss">
-body {
-  font: 20px "Helvetica", "Arial","LiHei Pro","黑體-繁","微軟正黑體", sans-serif;
-  margin: 20px;
-}
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  text-align: -webkit-center;
-}
+  body {
+    font: 20px "Helvetica", "Arial","LiHei Pro","黑體-繁","微軟正黑體", sans-serif;
+    margin: 20px;
+  }
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    text-align: -webkit-center;
+  }
 </style>
